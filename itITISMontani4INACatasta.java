c1a825394315f843341c94148ef7c3aad5f1cd09
GabrieleCat99 <omerussss@gmail.com>
2017-01-08 17:56:41 +0100

    Create itITISMontani4INACatasta.java

---
+++ itITISMontani4INACatasta.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package it.ITISMontani.pac4INA.alunni.catasta;
+
+/**
+ *
+ * @author Gabriele
+ */
+public class itITISMontani4INACatasta {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Ciao mondo!");
+        // TODO code application logic here
+    }
+    
+}
